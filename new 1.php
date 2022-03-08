cde565953e108d5a3ec526bd2fcec4c61da224d9
Alafia1 <101114171+Alafia1@users.noreply.github.com>
2022-03-08 15:59:08 +0100

    Update new 1.php
    Using If Statement, you can handle the issue of empty fields in your simple calculator

--- new 1.php
+++ new 1.php
@@ -10,8 +10,8 @@
 <body>
 
 <form>
-	<input type="text" name="num1" placeholder="Number 1">
-	<input type="text" name="num2" placeholder="Number 2">
+	<input type="number" name="num1" placeholder="Number 1">
+	<input type="number" name="num2" placeholder="Number 2">
 	<select name="operator">
 		<option>None</option>
 		<option>Add</option>
@@ -24,7 +24,7 @@
 	</select>
 	<br>
 	<button type=" submit" name="submit"  value="submit" >CALCULATE</button>
-	
+
 
 </form>
 
@@ -50,17 +50,33 @@ if(isset($_GET['submit'])) {
 			echo $result1 / $result2;
 		break;
 		case "Square":
-			echo $result1 * $result1;
+			echo pow($result1, 2);
 		break;
 		case "Cube":
-			echo $result1 * $result1 * $result1;
+			echo pow($result1, 3);
 		break;
 		case "Squareroot":
-			echo  sqrt ($result1) ;
+			if (empty($result1) && empty($result2)) {
+				// code...
+				echo "Please Enter A Number in Number 1 or Number 2";
+			}
+			elseif (empty($result1)) {
+				// code...
+				echo sqrt($result2);
+			}
+			elseif (Empty($result2)) {
+				// code...
+				echo sqrt($result1);
+			}
+			else {
+				// code...
+				echo  sqrt ($result1)." and ".sqrt($result2);
+			}
+			//echo  sqrt ($result1);
 		break;
-		
-		
-		
+
+
+
 	}
 }
 
